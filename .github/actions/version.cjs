8456e54c28043b38cd6bfddd24c01a0870eba8d3
swkeep <49286776+swkeep@users.noreply.github.com>
2024-02-09 18:22:32 +0300

    fix: version script

--- .github/actions/version.cjs
+++ .github/actions/version.cjs
@@ -4,7 +4,7 @@ const version = process.env.RELEASE_VERSION;
 const newVersion = version.replace('v', '');
 
 try {
-    let manifestFile = fs.readFileSync('fxmanifest.lua', 'utf8');
+    let manifestFile = fs.readFileSync('./interactionMenu/fxmanifest.lua', 'utf8');
     const newFileContent = manifestFile.replace(/\bversion\s+(.*)$/gm, `version '${newVersion}'`);
     fs.writeFileSync('fxmanifest.lua', newFileContent);
 } catch (err) {
