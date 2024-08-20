5fe6880c65f23c83f9350b1a96f057fba3557bba
swkeep <badaosoft@gmail.com>
2024-08-20 16:33:44 +0330

    chore: update main.js

--- interactionDUI/dui_source/src/main.js
+++ interactionDUI/dui_source/src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from 'vue';
+import App from './App.vue';
 
 import './reset.css';
 import './style.scss';
 
 // themes
 import './themes.scss';
-
-import App from './App.vue';
 
 createApp(App).mount('#app');
