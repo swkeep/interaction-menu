2ebb8e6453bc057637a4485f8383c4227c4a388c
swkeep <badaosoft@gmail.com>
2024-08-20 21:49:38 +0330

    Update stressTest.lua

--- interactionMenu/lua/examples/stressTest.lua
+++ interactionMenu/lua/examples/stressTest.lua
@@ -1,267 +1,171 @@
+-- Ensure DEVMODE is enabled to run tests
 if not DEVMODE then return end
 if true then return end
--- local function test()
---     local centerPoint = vector3(-2002.83, 3222.66, 32.81)
---     local positions = {}
---     local ids = {}
---     local numPoints = 20
---     local radius = 5.0
---     local direction = false
+-- calculate positions in a circle
+local function calculateCirclePositions(centerPoint, numPoints, radius, direction)
+    local positions = {}
+    for i = 1, numPoints do
+        local angle = (2 * math.pi / numPoints) * i
+        local x = centerPoint.x + radius * math.cos(angle)
+        local y = centerPoint.y + radius * math.sin(angle)
+        local heading = math.deg(math.atan(centerPoint.y - y, centerPoint.x - x))
+        if direction then
+            heading = heading - 90
+        else
+            heading = heading + 90
+        end
+        if heading < 0 then
+            heading = heading + 360
+        end
+        local spawnPoint = vector4(x, y, centerPoint.z, heading)
+        table.insert(positions, spawnPoint)
+    end
+    return positions
+end
 
---     for i = 1, numPoints do
---         local angle = (2 * math.pi / numPoints) * i
---         local x = centerPoint.x + radius * math.cos(angle)
---         local y = centerPoint.y + radius * math.sin(angle)
---         local heading = math.deg(math.atan(centerPoint.y - y, centerPoint.x - x))
---         if direction then
---             heading = heading - 90
---         else
---             heading = heading + 90
---         end
---         if heading < 0 then
---             heading = heading + 360
---         end
---         local spawnPoint = vector4(x, y, centerPoint.z, heading)
---         table.insert(positions, spawnPoint)
---     end
+-- Test for position menu creation
+local function test_position_menu()
+    local centerPoint = vector3(-2002.83, 3222.66, 32.81)
+    local numPoints = 20
+    local radius = 5.0
+    local direction = false
+    local positions = calculateCirclePositions(centerPoint, numPoints, radius, direction)
+    local ids = {}
 
---     for i = 1, numPoints, 1 do
---         local point = positions[i]
---         ids[#ids + 1] = exports['interactionMenu']:Create {
---             type = 'position',
---             position = vec3(point.x, point.y, point.z),
---             maxDistance = 2.0,
---             options = {
---                 {
---                     label = 'Menu|' .. i,
---                     icon = 'fas fa-car',
---                     action = {
---                         type = 'sync',
---                         func = function()
---                         end
---                     }
---                 },
---             }
---         }
---     end
+    for i, point in ipairs(positions) do
+        ids[#ids + 1] = exports['interactionMenu']:Create {
+            type = 'position',
+            position = vec3(point.x, point.y, point.z),
+            maxDistance = 2.0,
+            options = {
+                {
+                    label = 'Menu|' .. i .. " Test",
+                    icon = 'fas fa-car',
+                    action = {
+                        type = 'sync',
+                        func = function()
+                            print("Menu " .. i .. " triggered.")
+                        end
+                    }
+                },
+            }
+        }
+    end
 
---     Wait(50)
+    -- Wait and remove menus
+    Wait(1500)
+    for _, id in ipairs(ids) do
+        exports['interactionMenu']:remove(id)
+    end
+end
 
---     for index, value in ipairs(ids) do
---         exports['interactionMenu']:remove(value)
---     end
--- end
+-- Test for zone menu creation
+local function test_zone_menu()
+    local centerPoint = vector3(-2002.83, 3222.66, 32.81)
+    local numPoints = 10
+    local radius = 5.0
+    local direction = false
+    local positions = calculateCirclePositions(centerPoint, numPoints, radius, direction)
+    local ids = {}
 
--- local function test_zone()
---     local centerPoint = vector3(-2002.83, 3222.66, 32.81)
---     local positions = {}
---     local ids = {}
---     local numPoints = 100
---     local radius = 5.0
---     local direction = false
+    for i, point in ipairs(positions) do
+        ids[#ids + 1] = exports['interactionMenu']:Create {
+            position = point,
+            id = "zoneTest" .. i,
+            zone = {
+                type = 'boxZone',
+                name = "zoneTest" .. i,
+                position = point,
+                heading = point.w,
+                width = 4.0,
+                length = 6.0,
+                debugPoly = true,
+                minZ = point.z - 1,
+                maxZ = point.z + 1,
+            },
+            maxDistance = 2.0,
+            options = {
+                {
+                    label = 'Zone Menu|' .. i,
+                    icon = 'fas fa-car',
+                    action = function()
+                        print("Zone " .. i .. " triggered.")
+                    end,
+                    canInteract = function()
+                        return true
+                    end
+                },
+            }
+        }
+    end
 
---     for i = 1, numPoints do
---         local angle = (2 * math.pi / numPoints) * i
---         local x = centerPoint.x + radius * math.cos(angle)
---         local y = centerPoint.y + radius * math.sin(angle)
---         local heading = math.deg(math.atan(centerPoint.y - y, centerPoint.x - x))
---         if direction then
---             heading = heading - 90
---         else
---             heading = heading + 90
---         end
---         if heading < 0 then
---             heading = heading + 360
---         end
---         local spawnPoint = vector4(x, y, centerPoint.z, heading)
---         table.insert(positions, spawnPoint)
---     end
+    -- Wait and remove zones
+    Wait(4000)
+    for _, id in ipairs(ids) do
+        exports['interactionMenu']:remove(id)
+    end
+end
 
---     for i = 1, numPoints, 1 do
---         local point = positions[i]
---         ids[#ids + 1] = exports['interactionMenu']:Create {
---             position = point,
---             zone = {
---                 type = 'boxZone', -- entityZone/circleZone/polyZone/comboZone
---                 name = "onZoneTest" .. i,
---                 position = point,
---                 heading = point.w,
---                 width = 4.0,
---                 length = 6.0,
---                 debugPoly = true,
---                 minZ = point.z - 1,
---                 maxZ = point.z + 1,
---             },
---             maxDistance = 2.0,
---             options = {
---                 {
---                     label = 'Menu|' .. i,
---                     icon = 'fas fa-car',
---                     action = {
---                         type = 'sync',
---                         func = function()
---                         end
---                     }
---                 },
---             }
---         }
---     end
+-- Test for entity menu creation
+local function test_entity_menu()
+    local centerPoint = vector3(-2002.83, 3222.66, 31.81)
+    local numPoints = 25
+    local radius = 5.0
+    local direction = false
+    local positions = calculateCirclePositions(centerPoint, numPoints, radius, direction)
+    local entities = {}
+    local ids = {}
 
---     Wait(50)
+    for i, point in ipairs(positions) do
+        entities[i] = Util.spawnObject(`prop_vend_snak_01`, point)
 
---     for index, value in ipairs(ids) do
---         exports['interactionMenu']:remove(value)
---     end
--- end
+        ids[#ids + 1] = exports['interactionMenu']:Create {
+            entity = entities[i],
+            maxDistance = 2.0,
+            options = {
+                {
+                    label = 'Entity Menu|' .. i,
+                    icon = 'fas fa-car',
+                    action = {
+                        type = 'sync',
+                        func = function()
+                            print("Entity " .. i .. " triggered.")
+                        end
+                    }
+                },
+            }
+        }
+    end
 
--- local function test_onPlayer()
---     local ids = {}
---     local numPoints = 100
+    Wait(1000)
+    for _, id in ipairs(ids) do
+        exports['interactionMenu']:remove(id)
+    end
+    for _, entity in ipairs(entities) do
+        DeleteEntity(entity)
+    end
+end
 
---     for i = 1, numPoints, 1 do
---         ids[#ids + 1] = exports['interactionMenu']:create {
---             player = i,
---             offset = vec3(0, 0, 0),
---             maxDistance = 1.0,
---             options = {
---                 {
---                     label = 'Just On Player Id: ' .. i,
---                     icon = 'fa fa-person',
---                     action = {
---                         type = 'sync',
---                         func = function(data)
---                             Util.print_table(data)
---                         end
---                     }
---                 }
---             }
---         }
---     end
+CreateThread(function()
+    Wait(1000)
+    while true do
+        test_position_menu()
+        Wait(2000)
+    end
+end)
 
---     Wait(50)
+CreateThread(function()
+    Wait(1000)
+    while true do
+        test_zone_menu()
+        Wait(2000)
+    end
+end)
 
---     for index, value in ipairs(ids) do
---         exports['interactionMenu']:remove(value)
---     end
--- end
-
--- local function test_entity()
---     local centerPoint = vector3(-2002.83, 3222.66, 32.81)
---     local positions = {}
---     local ids = {}
---     local entities = {}
---     local numPoints = 30
---     local radius = 5.0
---     local direction = false
-
---     for i = 1, numPoints do
---         local angle = (2 * math.pi / numPoints) * i
---         local x = centerPoint.x + radius * math.cos(angle)
---         local y = centerPoint.y + radius * math.sin(angle)
---         local heading = math.deg(math.atan(centerPoint.y - y, centerPoint.x - x))
---         if direction then
---             heading = heading - 90
---         else
---             heading = heading + 90
---         end
---         if heading < 0 then
---             heading = heading + 360
---         end
---         local spawnPoint = vector4(x, y, centerPoint.z, heading)
---         table.insert(positions, spawnPoint)
---     end
-
---     for i = 1, numPoints, 1 do
---         entities[i] = Util.spawnObject(`prop_vend_snak_01`, positions[i])
-
---         ids[#ids + 1] = exports['interactionMenu']:Create {
---             entity = entities[i],
---             maxDistance = 2.0,
---             options = {
---                 {
---                     label = 'Menu|' .. i,
---                     icon = 'fas fa-car',
---                     action = {
---                         type = 'sync',
---                         func = function()
---                         end
---                     }
---                 },
---             }
---         }
---     end
-
---     Wait(500)
-
---     for index, value in ipairs(ids) do
---         exports['interactionMenu']:remove(value)
---     end
---     for index, value in ipairs(entities) do
---         DeleteEntity(value)
---     end
--- end
-
--- CreateThread(function()
---     Wait(1000)
-
---     while true do
---         test()
---         print(Container.count())
---         Wait(500)
---     end
--- end)
-
--- CreateThread(function()
---     Wait(1000)
---     local numPoints = 10000
-
---     for i = 1, 10, 1 do
---         for ij = 1, numPoints, 1 do
---             exports['interactionMenu']:Create {
---                 type = 'position',
---                 position = vec3(math.random(0, 5000), math.random(0, 5000), math.random(0, 5000)),
---                 maxDistance = 2.0,
---                 options = {
---                     {
---                         label = 'Menu|' .. ij,
---                         icon = 'fas fa-car',
---                         action = {
---                             type = 'sync',
---                             func = function()
---                             end
---                         }
---                     },
---                 }
---             }
---         end
---         Wait(500)
---     end
--- end)
-
--- CreateThread(function()
---     local veh_pos = vector4(-1974.9, 3178.76, 32.81, 59.65)
---     local vehicle = Util.spawnVehicle('adder', veh_pos)
-
---     while true do
---         for i = 1, 100, 1 do
---             local id = exports['interactionMenu']:Create {
---                 bone = 'platelight',
---                 vehicle = vehicle,
---                 offset = vec3(0, 0, 0),
---                 maxDistance = 2.0,
---                 indicator = {
---                     prompt   = 'E',
---                     keyPress = {
---                         -- https://docs.fivem.net/docs/game-references/controls/#controls
---                         padIndex = 0,
---                         control = 38
---                     }
---                 },
---                 options = {}
---             }
-
---             exports['interactionMenu']:remove(id)
---         end
---         Wait(50)
---     end
--- end)
+CreateThread(function()
+    Wait(1000)
+    while true do
+        test_entity_menu()
+        Wait(2000)
+    end
+end)
