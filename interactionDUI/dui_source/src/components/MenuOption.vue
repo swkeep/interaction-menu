0964d0bb6b91779b68e096a1ac6c942867c52b78
swkeep <badaosoft@gmail.com>
2024-08-22 20:12:20 +0330

    feat: add dompurify

--- interactionDUI/dui_source/src/components/MenuOption.vue
+++ interactionDUI/dui_source/src/components/MenuOption.vue
@@ -2,7 +2,7 @@
     <input v-if="isRadio" class="menu__option__radio" type="radio" :name="radioName" :checked="isSelected" />
     <div class="label" :class="labelClass" :style="computedItemStyle">
         <i v-if="item.icon" :class="[item.icon, 'label__icon']"></i>
-        <span v-if="!isRadio" v-html="item.label"></span>
+        <span v-if="!isRadio" v-html="sanitizedHTML"></span>
         <div v-if="isRadio">
             {{ item.label }}
         </div>
@@ -13,11 +13,19 @@
 import { computed } from 'vue';
 import { Option } from '../types/types';
 import { itemStyle } from '../util';
+import DOMPurify from 'dompurify';
 
 const props = defineProps<{
     item: Option;
     selected?: number; // when it's radio
 }>();
+
+const sanitizedHTML = computed(() => {
+    const html = props.item.label;
+    if (html === undefined) return '';
+
+    return DOMPurify.sanitize(html);
+});
 
 const isRadio = computed(
     () => (props.item.flags?.update || props.item.flags?.action || props.item.flags?.event) ?? false,
